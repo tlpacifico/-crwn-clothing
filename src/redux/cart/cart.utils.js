3572c7b5541d7b9f9e749f21acec4a0cf03d4183
Thacio Oliveira <thacio.pacfico@gmail.com>
2020-04-26 18:08:15 +0100

    added addItem and removeItem feature to app

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,3 +10,16 @@ export const addItemToCart = (cartItems, cartItemtoAdd) => {
 
     return [...cartItems, { ...cartItemtoAdd, quantity: 1 }]
 }
+
+export const removeItemToCart = (cartItems, cartItemtoRemove) => { 
+    const existingCartItem = cartItems.find(c => c.id === cartItemtoRemove.id);
+    
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(item => item.id !== cartItemtoRemove.id);
+    }
+
+    return cartItems.map(item => item.id === cartItemtoRemove.id
+        ? {...item, quantity: item.quantity - 1 }
+        : item 
+        );
+}
