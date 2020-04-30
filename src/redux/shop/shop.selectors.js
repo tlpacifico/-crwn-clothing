411d4e86312cffbbae5eeab11e12310fa397df9d
Thacio Oliveira <thacio.pacfico@gmail.com>
2020-04-30 23:44:48 +0100

    moved directory and shop data into its own reducr, created respective redux flow

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,20 @@
+import  { createSelector } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector(
+    [selectShop],
+    shop => shop.collection
+);
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = 
+collectionUrlPage => createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlPage]
+    
+);
